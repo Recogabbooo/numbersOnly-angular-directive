ccd65bcdcef5c5b6f0c08aa9add1312cacad0ab1
Bezael <bezael@gmail.com>
2021-12-24 11:09:10 +0100

    Add numbers only directive

---
+++ src/app/directives/numbers-only.directive.ts
@@ -0,0 +1,20 @@
+import { Directive, ElementRef, HostListener } from '@angular/core';
+
+@Directive({
+  selector: 'input[numbersOnly]'
+})
+export class NumbersOnlyDirective {
+
+  constructor(private readonly elRef: ElementRef) { }
+  @HostListener('input', ['$event'])
+  onChangeInput(event: Event): void {
+    const numbersOnly = /[^0-9]*/g
+
+    const initValue = this.elRef.nativeElement.value;
+    this.elRef.nativeElement.value = initValue.replace(numbersOnly, '')
+    if (initValue !== this.elRef.nativeElement.value) {
+      event.stopPropagation();
+    }
+  }
+
+}
